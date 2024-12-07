dd49e0f2cd0687dae61198798758365155dd4130
ssmahim01 <mahimzayn666@gmail.com>
2024-12-07 12:07:34 +0600

    Updated Home page and AllReviews page with Mindset Section also Modified config file of Tailwind

--- src/components/MindsetSection.jsx
+++ src/components/MindsetSection.jsx
@@ -9,8 +9,8 @@ const MindsetSection = () => {
                 src="pro-video-game-r-losing-space-shooter-competition-while-wearing-virtual-reality-headset.jpg" alt="An Image of Pro Video Gamer" />
             </figure>
 
-            <div className="md:w-1/2 lg:space-y-3 space-y-1 lg:px-10 px-5 *:md:text-left *:text-center lg:mb-10 mb-6">
-            <h2 className="lg:text-4xl md:text-3xl text-2xl text-purple-700 font-extrabold">Pro Video Gamer</h2>
+            <div className="md:w-1/2 lg:space-y-4 space-y-1 lg:px-10 px-5 *:md:text-left *:text-center lg:mb-10 mb-6">
+            <h2 className="lg:text-5xl md:text-3xl text-2xl text-purple-700 font-extrabold">Pro Video Gamer</h2>
             <p className="text-gray-600 lg:text-base md:text-sm font-medium">A serious pro gamer was joined in competition. Unfortunately, she cannot won the game. She has game mindset. Like her, any of pro players are play video games as their profession.</p>
             </div>
         </div>
--- src/pages/AllReviews.jsx
+++ src/pages/AllReviews.jsx
@@ -1,8 +1,30 @@
 import { useEffect, useState } from "react";
 import AllReviewsCard from "../components/AllReviewsCard";
+import { Typewriter } from "react-simple-typewriter";
 
 const AllReviews = () => {
   const [reviews, setReviews] = useState([]);
+  const [sortItems, setSortItems] = useState("");
+
+  const handleSorted = (type) => {
+    setSortItems(type);
+
+    let sortBy = "";
+
+    if(type == "Rating"){
+      sortBy = "rating";
+    };
+
+    if(type == "Publish Year"){
+      sortBy = "publishingYear";
+    };
+
+    fetch(`http://localhost:4500/reviews?sortBy=${sortBy}`)
+        .then((response) => response.json())
+        .then((data) => {
+          setReviews(data);
+        });
+  };
 
   useEffect(() => {
     fetch("http://localhost:4500/reviews")
@@ -14,7 +36,56 @@ const AllReviews = () => {
 
   return (
     <div className="lg:w-4/5 md:w-11/12 w-full mx-auto my-14">
-      <h2 className="md:text-5xl text-4xl font-bold text-center mb-8">All Reviews</h2>
+      <div className="flex justify-between items-center mb-10">
+        <h2 className="md:text-4xl text-3xl font-extrabold">
+          All Reviews{" "}
+          <span className="text-primary font-bold">
+            <Typewriter
+              words={[
+                "Action",
+                "RPG",
+                "Sports",
+                "Adventure",
+                "Battle",
+                "Puzzle",
+              ]}
+              loop={false}
+              cursor
+              cursorStyle="|"
+              typeSpeed={70}
+              deleteSpeed={40}
+              delaySpeed={1000}
+            />
+          </span>
+        </h2>
+
+        <div className="dropdown">
+          <div
+            tabIndex={0}
+            role="button"
+            className="btn bg-cyan-600 m-1 text-white font-bold px-6"
+          >
+            {sortItems ? `${sortItems}`:"Sort By"}
+          </div>
+          <ul
+            tabIndex={0}
+            className="dropdown-content menu bg-base-200 rounded-box z-[1] w-56 p-4 shadow-md space-y-2 *:font-bold"
+          >
+            <li
+              className="cursor-pointer hover:btn-active p-2"
+              onClick={() => handleSorted('Rating')}
+            >
+              Rating
+            </li>
+            <li
+              className="cursor-pointer hover:btn-active p-2"
+              onClick={() => handleSorted('Publish Year')}
+            >
+              Publish Year
+            </li>
+          </ul>
+        </div>
+      </div>
       <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
         {reviews.map((review) => (
           <AllReviewsCard key={review._id} review={review}></AllReviewsCard>
